1aaef895be861ffb4355fd83676fa2991637f96f
Jonathan Lai <jonathanlai@eltux.net>
2013-05-25 23:54:37 -0700

    Added chat-app, still need to troubleshoot

---
+++ chat-app/README.md
@@ -0,0 +1,2 @@
+From following Real Time Chat With NodeJS, Socket.io and ExpressJS on 
+http://net.tutsplus.com/tutorials/javascript-ajax/real-time-chat-with-nodejs-socket-io-and-expressjs/
---
+++ chat-app/index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var port = 1234;
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function(socket) {
+  socket.emit('message', { message: 'Welcome to the Chat!' });
+  socket.on('send', function(data) {
+    io.socket.emit('message', data);
+  });
+});
+
+
+
+console.log("Listening on port " + port);
+
