8b7244c785515688931844e683a99b3231e65907
Jonathan Lai <jonathanlai@eltux.net>
2013-05-26 16:19:27 -0700

    update chat app

--- chat-app/index.js
+++ chat-app/index.js
@@ -17,11 +17,9 @@ var io = require('socket.io').listen(app.listen(port));
 io.sockets.on('connection', function(socket) {
   socket.emit('message', { message: 'Welcome to the Chat!' });
   socket.on('send', function(data) {
-    io.socket.emit('message', data);
+    io.sockets.emit('message', data);
   });
 });
-
-
 
 console.log("Listening on port " + port);
 
